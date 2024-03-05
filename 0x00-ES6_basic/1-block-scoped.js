7501b8e191bd2db9cf3a2c7b8c599ad99c857dbe
eungobs <eungobs@gmail.com>
2024-03-05 12:17:37 +0000

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;  // Remove 'var' here
-    task2 = false;  // Remove 'var' here
+    // eslint-dasable-next-line no-unsused-vars, no-shadow
+    const task = true;
+    // eslint-disable-next-line no-unused-vars, no-shadow	  
+    const task2 = false;
   }
 
   return [task, task2];
