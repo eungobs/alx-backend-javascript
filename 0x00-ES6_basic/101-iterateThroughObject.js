a2992873a8b1f42a06be7aff551e212454a7db84
eungobs <eungobs@gmail.com>
2024-03-05 12:57:22 +0000

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  let employee;
-  while ((employee = reportWithIterator.next().value) !== undefined) {
-    result += employee + ' | ';
-  }
-  // Remove the trailing ' | ' if there are employees
-  if (result.length > 0) {
-    result = result.slice(0, -3);
-  }
-  return result;
+  return reportWithIterator.join(' | ');
 }
